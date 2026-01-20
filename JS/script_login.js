64bc0b7d72398bbc6ee58ef8c4678ee2d5e2239c
estiven-piedrahita <duvanestivenpiedrahita@gmail.com>
2026-01-19 19:39:47 -0500

    termine el js de el login y register

--- JS/script_login.js
+++ JS/script_login.js
@@ -3,18 +3,6 @@ let inputEmail = document.getElementById("Email");
 let password = document.getElementById("password");
 let p = document.getElementById("message");
 
-function the_user() {
-if (!localStorage.getItem("users")) {
-    let users = [
-        { email: "duvanpiedrahita@gmail.com", password: "123" },
-        { email: "duvancito@gmail.com", password: "1212" },
-        {email:"duvi@gmail.com", password: "duvi"},
-    ];
-
-    localStorage.setItem("users", JSON.stringify(users));
-}
-}
-the_user();
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
 
@@ -23,29 +11,27 @@ form.addEventListener("submit", (e)=> {
 
     let users = JSON.parse(localStorage.getItem("users")) || [];
     
-    let userFound =  false;
+    let userFound = users.find(user => user.email === theEmail && user.password === the_password
+    );
 
-
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].email === theEmail && users[i].password === the_password) {
-            userFound = true;
-
-            localStorage.setItem("loggedUser",theEmail);
-            break;
-        }
-    }
-    if (userFound) {
-        p.textContent = "Login successful";
-        p.style.color = "green";
-        setTimeout(()=>{
-            window.location.href = "profiles.html"
-        },1000)
-        
-    }
-    else{
-        p.textContent = "Wrong email or password";  
+    if (!userFound) {
+        p.textContent = "Invalid email or password";
         p.style.color = "red";
+        setTimeout(() => p.textContent = "", 2000);
+        form.reset();
+        return;
     }
+
+
+    localStorage.setItem("loggedUser", JSON.stringify(userFound));
+    
+    p.textContent = "Login successful";
+    p.style.color = "green"
+
+    setTimeout(() => {
+    window.location.href = "home.html";
+}, 1000);
+
 });
 
 
--- JS/script_register.js
+++ JS/script_register.js
@@ -1,1 +1,41 @@
-diofgiojf
+let form = document.getElementById("registerForm");
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+let inputEmail = document.getElementById("email").value;
+let inputPassword = document.getElementById("password").value;
+let confirmPassword = document.getElementById("ConfirmPassword").value;
+let p = document.getElementById("message");
+
+let existEmail = users.some(user => user.email === inputEmail);
+
+if (existEmail) {
+    alert("You already have an account created");
+    return;
+}
+console.log(inputPassword, confirmPassword);
+
+if (inputPassword !== confirmPassword) {
+    p.textContent = "The passwords do not match.";
+    p.style.color = "red";
+    setTimeout(() => p.textContent = "", 2000);
+    return;
+}
+users.push({
+    "email": inputEmail,
+    "password": inputPassword
+});
+localStorage.setItem("users", JSON.stringify(users));
+
+localStorage.setItem("loggedUser",inputEmail);
+
+alert("Account successfully created");
+    p.textContent = "Account successfully created";
+    p.style.color = "green";
+
+    window.location.href = "login.html";
+    form.reset();
+});
